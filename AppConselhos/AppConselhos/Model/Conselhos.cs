9d68cb5e958b7b46e0b8f6b1de398595e5a1ed1b
Usuario <Usuario@CPD02-M12>
2022-10-18 12:16:19 -0300

    Adicionar arquivos de projeto.

---
+++ AppConselhos/AppConselhos/Model/Conselhos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppConselhos.Model
+{
+     class Conselhos
+    {
+        public string Id { get; set; }
+        public string Frase { get; set; }
+
+        public Conselhos()
+        {
+            this.Id = " ";
+
+            this.Frase = " ";
+
+        }
+    }
+
+    
+}
---
+++ AppConselhos/AppConselhos/Services/DataService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AppConselhos.Model;
+using System.Data;
+
+namespace AppConselhos.Services
+{
+    class DataService
+    {
+        public static async Task<Conselhos> GetConselhosTwitter()
+        {
+            string queryString = "https://api.adviceslip.com/advice";
+            dynamic resultado = await GetDataFromService(queryString).ConfigureAwait(false);
+
+            if (resultado["Conselhos"] !=null)
+            {
+                Conselhos conselhos = new Conselhos();
+                conselhos.Id = (string) resultado ["id"];
+                conselhos.Frase = (string)resultado["Conselhinhos"];
+                return conselhos;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static async Task<dynamic> GetDataFromService(string queryString)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(queryString);
+            dynamic data = null;
+            if (response != null)
+            {
+                string json =  response.Content.ReadAsStringAsync().Result;
+                data = JsonConvert.DeserializeObject<dynamic>(json);
+            }
+            return data;
+        }
+    }
+}
