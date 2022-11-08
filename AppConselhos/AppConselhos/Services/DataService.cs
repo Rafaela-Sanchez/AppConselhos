a498589c5b968e0e71f170350bea09dc3e5194a7
Usuario <Usuario@CPD02-M12>
2022-11-08 12:26:56 -0300

    Mudanças DataService

--- AppConselhos/AppConselhos/Services/DataService.cs
+++ AppConselhos/AppConselhos/Services/DataService.cs
@@ -35,12 +35,31 @@ namespace AppConselhos.Services
             HttpClient client = new HttpClient();
             var response = await client.GetAsync(queryString);
             dynamic data = null;
+
             if (response != null)
             {
                 string json =  response.Content.ReadAsStringAsync().Result;
                 data = JsonConvert.DeserializeObject<dynamic>(json);
             }
             return data;
+        }
+
+        public static async Task<dynamic> GetAdviceByNum(string num)
+        {
+            string queryString = "https://api.adviceslip.com/advice/" + num;
+            dynamic resultado = await GetDataFromService(queryString).ConfigureAwait(false);
+
+            if (resultado["Conselhos"] != null)
+            {
+                Conselhos conselhos = new Conselhos();
+                conselhos.Id = (string)resultado["id"];
+                conselhos.Frase = (string)resultado["Conselhinhos"];
+                return conselhos;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
